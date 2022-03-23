eee49cc07ad2367e7f59ee56d3c880c4ffb264f6
Yunyun Liu <celeph4is@gmail.com>
2022-03-22 19:22:48 -0700

    render li elements containing ToDoItem components from an array of todo data

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,29 @@
   <div id='id'>
     <h1>todo list</h1>
     <ul>
-      <li>
-        <to-do-item label="first todo item" :done="false"></to-do-item>
-      </li>
-      <li>
-        <to-do-item label="second todo item" :done="true"></to-do-item>
-      </li>
-      <li>
-        <to-do-item label="third todo item" :done="true"></to-do-item>
-      </li>
-      <li>
-        <to-do-item label="fourth todo item" :done="false"></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+  import uniqueId from 'lodash.uniqueid';
   import ToDoItem from './components/ToDoItem.vue'
   export default {
     name: 'App',
-    components: { ToDoItem }
+    components: { ToDoItem },
+    data() {
+      return {
+        ToDoItems: [
+          { label: 'Learn Vue', done: false, id: uniqueId('todo-') },
+          { label: 'Create a Vue project with theCLI', id: uniqueId('todo-') },
+          { label: 'Have fun', id: uniqueId('todo-') },
+          { label: 'Create a to-do list', done: false, id: uniqueId('todo-') }
+        ]
+      }
+    }
   }
 </script>
 
@@ -34,5 +36,9 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  ul {
+    list-style-type: none;
   }
 </style>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -6,16 +6,15 @@
 </template>
 
 <script>
-  import uniqueId from 'lodash.uniqueid';
   export default {
     props: {
       label: { required: true, type: String },
-      done: { default: false, type: Boolean }
+      done: { default: false, type: Boolean },
+      id: { required: true, type: String }
     },
     data() {
       return {
-        isDone: this.done,
-        id: uniqueId('todo-')
+        isDone: this.done
       }
     }
   }
